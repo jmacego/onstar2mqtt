5bcdb1f0c1d6ce2d844ae916da242630b8c6c7be
BigThunderSR <17056173+BigThunderSR@users.noreply.github.com>
2025-04-04 13:30:37 -0500

    Add comment for future reference

--- src/mqtt.js
+++ src/mqtt.js
@@ -152,7 +152,7 @@ class MQTT {
             case 'PRIORITY CHARGE INDICATOR':
             case 'PRIORITY CHARGE STATUS':
             case 'LOC BASED CHARGING HOME LOC STORED':
-            case 'SCHEDULED CABIN PRECONDTION CUSTOM SET REQ ACTIVE':
+            case 'SCHEDULED CABIN PRECONDTION CUSTOM SET REQ ACTIVE': // There is a typo in the data coming from the API; 'PRECONDTION' is missing an 'i'.
             case 'VEH IN HOME LOCATION':
             case 'VEH NOT IN HOME LOC':
             case 'VEH LOCATION STATUS INVALID':
@@ -691,7 +691,7 @@ class MQTT {
                 case 'LOC BASED CHARGING HOME LOC STORED': // FALSE/TRUE
                     value = e.value === 'TRUE';
                     break;
-                case 'SCHEDULED CABIN PRECONDTION CUSTOM SET REQ ACTIVE': // FALSE/TRUE
+                case 'SCHEDULED CABIN PRECONDTION CUSTOM SET REQ ACTIVE': // FALSE/TRUE - There is a typo in the data coming from the API; 'PRECONDTION' is missing an 'i'.
                     value = e.value === 'TRUE';
                     break;
                 case 'VEH IN HOME LOCATION': // FALSE/TRUE
@@ -828,7 +828,7 @@ class MQTT {
             case 'PRIORITY CHARGE INDICATOR': // FALSE/TRUE
             case 'PRIORITY CHARGE STATUS': // NOT_ACTIVE/ACTIVE
             case 'LOC BASED CHARGING HOME LOC STORED': // FALSE/TRUE
-            case 'SCHEDULED CABIN PRECONDTION CUSTOM SET REQ ACTIVE': // FALSE/TRUE - There is a typo in the data coming from the API 'PRECONDTION' is missing an 'i'.
+            case 'SCHEDULED CABIN PRECONDTION CUSTOM SET REQ ACTIVE': // FALSE/TRUE - There is a typo in the data coming from the API; 'PRECONDTION' is missing an 'i'.
             case 'VEH IN HOME LOCATION': // FALSE/TRUE
             case 'VEH NOT IN HOME LOC': // FALSE/TRUE
             case 'VEH LOCATION STATUS INVALID': // FALSE/TRUE
